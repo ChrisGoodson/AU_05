9fca7cd83ace3763fe7d21828323648e1464f8ec
Chris <f.chrisgoodson@icloud.com>
2023-02-13 23:50:04 -0600

    added NavigationViewController, attached button to move to new controller.

--- AU_05/ViewController.swift
+++ AU_05/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nativage(_ sender: UIButton) {
+        let nativateController = NavigationViewController()
+        self.present(nativateController, animated: true, completion: nil)
+    }
+    
 }
 
